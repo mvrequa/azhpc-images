b8a2727d1cb51037cd7baafba1b08f18330c7abd
Michael Requa <michael.requa@microsoft.com>
2021-03-29 10:29:44 -0700

    image 1

--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc-ndv4/build.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc-ndv4/build.sh
@@ -24,4 +24,7 @@ wget "https://releases.hashicorp.com/packer/1.7.0/packer_1.7.0_linux_amd64.zip"
 unzip packer_1.7.0_linux_amd64.zip
 ls -lt
 pwd
-./packer build build.json
+
+SUB_ID=`az account show --query id | tr -d '"'`
+./packer build build.json \
+    -var "subscription_id=$SUB_ID"
