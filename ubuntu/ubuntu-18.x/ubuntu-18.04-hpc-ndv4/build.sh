ef03d6118adb38f47cd3d926f4e4ba2efe5bcc17
Michael Requa <michael.requa@microsoft.com>
2021-03-29 09:45:39 -0700

    image 1

--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc-ndv4/build.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc-ndv4/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-which azcopy
+#wget "https://releases.hashicorp.com/packer/1.7.0/packer_1.7.0_linux_amd64.zip"
 
-wget "https://releases.hashicorp.com/packer/1.7.0/packer_1.7.0_linux_amd64.zip"
-
+az storage blob download --account-name "requawestus2" \
+    --container-name "stor" \
+    --name \ packer_1.7.0_linux_amd64.zip
+    --file packer_1.7.0_linux_amd64.zip
+ 
 unzip packer_1.7.0_linux_amd64.zip
 ls -lt
 pwd
-./packer_1.7.0_linux_amd64/packer build build.json
+./packer build build.json
