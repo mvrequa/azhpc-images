afaf587e5174595d4b9c2a6336aab5ed11da755e
Michael Requa <michael.requa@microsoft.com>
2021-03-29 10:17:02 -0700

    image 1

--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc-ndv4/build.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc-ndv4/build.sh
@@ -3,22 +3,24 @@
 #wget "https://releases.hashicorp.com/packer/1.7.0/packer_1.7.0_linux_amd64.zip"
 
 
-end=`date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ'`
-/usr/bin/az storage container generate-sas \
-    --account-name "requawestus2" \
-    --name "stor" \
-    --permissions acdlrw \
-    --auth-mode login \
-    --as-user \
-    --expiry $end
+#end=`date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ'`
+#/usr/bin/az storage container generate-sas \
+#    --account-name "requawestus2" \
+#    --name "stor" \
+#    --permissions acdlrw \
+#    --auth-mode login \
+#    --as-user \
+#    --expiry $end
+#
+#/usr/bin/az storage blob download \
+#    --account-name "requawestus2" \
+#    --container-name "stor" \
+#    --name packer_1.7.0_linux_amd64.zip \
+#    --file packer_1.7.0_linux_amd64.zip \
+#    --sas-token $SAS_TOKEN
 
-/usr/bin/az storage blob download \
-    --account-name "requawestus2" \
-    --container-name "stor" \
-    --name packer_1.7.0_linux_amd64.zip \
-    --file packer_1.7.0_linux_amd64.zip \
-    --sas-token $SAS_TOKEN
- 
+
+wget "https://releases.hashicorp.com/packer/1.7.0/packer_1.7.0_linux_amd64.zip"
 unzip packer_1.7.0_linux_amd64.zip
 ls -lt
 pwd
